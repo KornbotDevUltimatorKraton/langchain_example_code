88e1269cdc10646a25582d78646a8fd3ab7b3144
Chanapai Chuadchum <kornbot359@hotmail.com>
2023-06-08 18:14:50 +0700

    Update langchain_test_data.py

--- langchain_test_data.py
+++ langchain_test_data.py
@@ -1,7 +1,7 @@
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
-secret_key="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"
+secret_key="Put your secretkey from openAI website"
 llm = OpenAI(openai_api_key=secret_key)
 text = "How to do trademark in USA?"
 print(llm(text))
